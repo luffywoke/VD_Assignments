fe4aaad09701ab1fb0f5c2edfb5b846420d7e70e
gabeRam <gaberam310@gmail.com>
2026-02-18 07:47:06 -0800

    Adding modified files

--- Assets/Assignments/PhysicsIntro2/Finish.cs
+++ Assets/Assignments/PhysicsIntro2/Finish.cs
@@ -2,10 +2,15 @@ using UnityEngine;
 
 public class Finish : MonoBehaviour
 {
+    private GameObject[] finishLine;
+    private GameObject[] spheres;
+
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        finishLine = GameObject.FindGameObjectsWithTag("Finish");
+        spheres = GameObject.FindGameObjectsWithTag("Sphere");
     }
 
     // Update is called once per frame
